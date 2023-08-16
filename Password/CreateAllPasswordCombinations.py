c1548f5b8d7ee26c81098ef7b2f807d2882d8582
Turnrp <70816015+Turnrp@users.noreply.github.com>
2023-08-16 14:20:03 -0400

    Update CreateAllPasswordCombinations.py

--- Password/CreateAllPasswordCombinations.py
+++ Password/CreateAllPasswordCombinations.py
@@ -5,18 +5,21 @@ from itertools import combinations
 alphabet = list(ascii_lowercase + ascii_uppercase + digits + "!@#$%^&*()")
 
 
-def generate_combinations(word_length):
+def generate_combinations(word_length, print_each):
     for combination in combinations(alphabet, word_length):
+        if print_each:
+            print(combination)
         yield "".join(combination)
 
 
-def save_combinations_to_file(word_length):
+def save_combinations_to_file(word_length, print_each):
     file_name = f"words{word_length}.txt"
     with open(file_name, "w") as file:
-        for word in generate_combinations(word_length):
+        for word in generate_combinations(word_length, print_each):
             file.write(word + "\n")
 
 
 if __name__ == "__main__":
+    print_each = input("Print Each Password? y/n\n") == "y"
     word_length = int(input("Length: "))
-    save_combinations_to_file(word_length)
+    save_combinations_to_file(word_length, print_each)
